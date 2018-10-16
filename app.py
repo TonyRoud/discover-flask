d4b2c3195e6070da51f1de6f62be4b0c450e24ea
Tony Roud <BonesUK@users.noreply.github.com>
2018-10-16 19:14:29 +0100

    added db and connection objects

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from functools import wraps
+import sqlite3
+
 # create the application project
 app = Flask(__name__)
 
 # Key should be random, and moved to separate file for security
 app.secret_key = "my precious"
+app.database = "sample.db"
+
 
 # login required decorator
 def login_required(f):
@@ -23,7 +27,11 @@ def login_required(f):
 @login_required
 def home():
     # return "Hello, world!" # return a string
-    return render_template("index.html")  # render a template
+    g.db = connect_db()  # g is a flask tool for storing temp objects
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', posts=posts)  # render a template
 
 @app.route('/welcome')
 def welcome():
@@ -48,7 +56,8 @@ def logout():
     flash('You successfully logged out')
     return redirect(url_for('welcome'))
 
-
+def connect_db():
+    return sqlite3.connect(app.database)
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
